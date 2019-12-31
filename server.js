4390d80069127ffd09fe99098c4ecc8a68c6aa82
Patrick Gustafson <patrickgus@charter.net>
2019-12-31 10:22:44 -0800

    create token validation function

--- server.js
+++ server.js
@@ -1,3 +1,4 @@
+require("dotenv").config();
 const express = require("express");
 const morgan = require("morgan");
 const MOVIEDEX = require("./moviedex.json");
@@ -5,6 +6,17 @@ const MOVIEDEX = require("./moviedex.json");
 const app = express();
 
 app.use(morgan("dev"));
+
+app.use(function validateBearerToken(req, res, next) {
+  const apiToken = process.env.API_TOKEN;
+  const authToken = req.get("Authorization");
+
+  if (!authToken || authToken.split(" ")[1] !== apiToken) {
+    return res.status(401).json({ error: "Unauthorized request" });
+  }
+
+  next();
+});
 
 function handleGetMovie(req, res) {
   res.json(MOVIEDEX.movies);
