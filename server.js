3c5976af9fccd00bb1077fe0ead7eee4f838d316
Patrick Gustafson <patrickgus@charter.net>
2020-01-06 15:21:33 -0800

    create error-handling middleware function

--- server.js
+++ server.js
@@ -49,6 +49,16 @@ function handleGetMovie(req, res) {
 
 app.get("/movie", handleGetMovie);
 
+app.use((error, req, res, next) => {
+  let response;
+  if (process.env.NODE_ENV === "production") {
+    response = { error: { message: "server error" } };
+  } else {
+    response = { error };
+  }
+  res.status(500).json(response);
+});
+
 const PORT = process.env.PORT || 8000;
 
 app.listen(PORT, () => {
